7494178bab251c00da1fa2a9f25fcca68141cfcf
adamtossell <adamtossell@gmail.com>
2025-03-12 12:35:20 +0000

    Add triangle to screen

--- main.py
+++ main.py
@@ -3,23 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        # fill the screen with black
         screen.fill((0,0,0))
+        # draw the player
+        player.draw(screen)
         pygame.display.flip()
         # limit the frame rate to 60 fps
         dt = clock.tick(60) / 1000
